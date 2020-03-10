808a20a11fb56b2e77ac6c194cead54f52e54ed6
Naman Agarwal <f20180435@pilani.bits-pilani.ac.in>
2020-03-10 15:23:27 +0530

    updated

--- app/src/main/java/com/example/android/apogeeportal/HomeFragment.java
+++ app/src/main/java/com/example/android/apogeeportal/HomeFragment.java
@@ -1,38 +1,45 @@
 package com.example.android.apogeeportal;
 
-import android.content.Intent;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.SetOptions;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class HomeFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
-    private Spinner spinner;
-    private static final String[] paths = {"Pick An event to modify", "item 2", "item 3"};
+    private Spinner modifyEventSpinner, eventTypeSpinner;
+    private static final String[] allEvents = {"Pick An event to modify", "item 2", "item 3"};
+    Button btnDatePicker, btnTimePicker;
+    EditText txtDate, txtTime;
+    private int mYear, mMonth, mDay, mHour, mMinute;
+    private static final String[] eventTypes = {"Category Of Event", "coding and fintech", "quizzing and strategy", "mechatronics", "civil", "electronics and robotics", "sciences", "others", "live events"};
+
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
 
@@ -41,46 +48,128 @@ public class HomeFragment extends Fragment implements AdapterView.OnItemSelected
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_home, null);
 
-        Button addEventButton = (Button) view.findViewById(R.id.add_event_btn);
-        addEventButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                EditText text = (EditText) view.findViewById(R.id.event_name_edit_text);
-                String eventName = text.getText().toString();
+        eventTypeSpinner = (Spinner) view.findViewById(R.id.eventTypeSpinner);
+        final ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(getContext(),
+                android.R.layout.simple_spinner_item, eventTypes);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        eventTypeSpinner.setAdapter(adapter1);
+        eventTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
 
-                //code to enter event in database
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
+            {
+                if (parent.getItemAtPosition(position).equals("Category Of Event"))
+                {
+                    //do nothing.
+                }
+                else
+                {
+                    // write code on what you want to do with the item selection
+                }
+            }
 
-                // Create a new user with a first and last name
-                Map<String, Object> user = new HashMap<>();
-                user.put("first", "Ada");
-                user.put("last", "Lovelace");
-                user.put("born", 1815);
-
-// Add a new document with a generated ID
-                db.collection("naman")
-                        .add(user)
-                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                            @Override
-                            public void onSuccess(DocumentReference documentReference) {
-                                Log.d("here","here");
-                                }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.d("failed here","failed here");
-                                }
-                        });
-
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
 
             }
         });
 
-        spinner = (Spinner) view.findViewById(R.id.spinner);
+        btnDatePicker = (Button) view.findViewById(R.id.btn_date);
+        btnTimePicker = (Button) view.findViewById(R.id.btn_time);
+        txtDate = (EditText) view.findViewById(R.id.in_date);
+        txtTime = (EditText) view.findViewById(R.id.in_time);
+
+        btnDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Get Current Date
+                final Calendar c = Calendar.getInstance();
+                mYear = c.get(Calendar.YEAR);
+                mMonth = c.get(Calendar.MONTH);
+                mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(),
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+
+                                txtDate.setText(year + "-" + (monthOfYear + 1) + "-" + dayOfMonth + "T");
+
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
+            }
+        });
+        btnTimePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Get Current Time
+                final Calendar c = Calendar.getInstance();
+                mHour = c.get(Calendar.HOUR_OF_DAY);
+                mMinute = c.get(Calendar.MINUTE);
+
+                // Launch Time Picker Dialog
+                TimePickerDialog timePickerDialog = new TimePickerDialog(getContext(),
+                        new TimePickerDialog.OnTimeSetListener() {
+
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay,
+                                                  int minute) {
+
+                                txtTime.setText(hourOfDay + ":" + minute + ":00Z");
+                            }
+                        }, mHour, mMinute, false);
+                timePickerDialog.show();
+            }
+        });
+
+        Button addEventButton = (Button) view.findViewById(R.id.add_event_btn);
+        addEventButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText eventNameTextView = (EditText) view.findViewById(R.id.event_name_edit_text);
+                String eventName = eventNameTextView.getText().toString();
+
+                //code to enter event in database
+                Map<String, Object> events = new HashMap<>();
+                events.put("Name", eventName);
+                events.put("Time", getDateFromString(txtDate.getText().toString() + txtTime.getText().toString()));
+                events.put("Type", eventTypeSpinner.getSelectedItem().toString());
+                events.put("lat", 28.36592);
+                events.put("long", 75.588224);
+
+// Add a new document with a generated ID
+                db.collection("Naman")
+                        .add(events)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Toast.makeText(getContext(), "Event Added", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(getContext(), "Failed to add Event", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    //Make all texts clear
+                        eventNameTextView.setText("");
+                        txtDate.setText("");
+                        txtTime.setText("");
+                        //eventTypeSpinner.setSelection(adapter1.getPosition());
+            }
+        });
+
+        modifyEventSpinner = (Spinner) view.findViewById(R.id.modifyEventSpinner);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),
-                android.R.layout.simple_spinner_item, paths);
+                android.R.layout.simple_spinner_item, allEvents);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(this);
+        modifyEventSpinner.setAdapter(adapter);
+        modifyEventSpinner.setOnItemSelectedListener(this);
 
         Button ModifyEventButton = (Button) view.findViewById(R.id.modify_event_btn);
         ModifyEventButton.setOnClickListener(new View.OnClickListener() {
@@ -88,6 +177,7 @@ public class HomeFragment extends Fragment implements AdapterView.OnItemSelected
                 // Do something in response to button click
             }
         });
+
 
         return view;
     }
@@ -101,4 +191,20 @@ public class HomeFragment extends Fragment implements AdapterView.OnItemSelected
     public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
+
+    //methods for date and time selection
+    final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+
+    public Date getDateFromString(String datetoSaved) {
+
+        try {
+            Date date = format.parse(datetoSaved);
+            return date;
+        } catch (ParseException e) {
+            return null;
+        }
+
+    }
+
+
 }
